b85fa547212e5e8a63997470540debecdd9f6fe1
ramugollapudi <ramu.gollapudi@laalsa.com>
2019-05-28 16:24:15 +0530

    git add commited data

--- modules/utilsV1/index.js
+++
@@ -1,9 +0,0 @@
-const serviceOptions = require('./serviceCallOptions');
-const serviceCaller = require('./serviceCaller');
-const asyncMongoose = require('./mongooseAsyncRepo');
-
-module.exports = {
-  serviceCaller,
-  serviceOptions,
-  asyncMongoose,
-};
